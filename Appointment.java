b692e41eb4a2472ddb6258115f0c92ee762ee360
esalik <salikemre65@gmail.com>
2021-05-08 15:38:50 +0300

    Added appointment class and method declarations

---
+++ Appointment.java
@@ -0,0 +1,48 @@
+import java.util.date;
+
+public class Appointment {
+    private Date appday;
+    private Boolean awake=True;
+
+
+    public Appointment(Doctor input_DOCTOR, Patient input_PATIENT,  Date input_DATE){
+        // Default Constructor with date datatype
+    }
+
+    public Appointment(Doctor input_DOCTOR, Patient input_PATIENT, int year, int month, int day, int hour){
+        //  Default constructor with integer inputs
+    }
+
+    public Boolean isAwake(){
+        // Method that will return if appointment is checked out by Patient or by Doctor or not
+            return awake;
+    }
+
+    public void checkout(){
+        // Method to access awake data
+    }
+
+    public Date getDate(){
+        // Returning appointment time
+    }
+
+    public Doctor getDoctor(){
+        // Returning assigned doctor class
+    }
+
+    public Patient getPatient(){
+        // Returning assigned patient class
+    }
+
+    public int compareAppointmentbyHour(Appointment appointment2){
+        // Comparing two appointment by their Date data
+        // Returns 0 if equal or interval in hours
+    }
+
+    public int compareAppointmentbyDay(Appointment appointment2){
+        // Comparing two appointment by their Date data
+        // Returns 0 if equal or interval in date
+    }
+
+
+}
