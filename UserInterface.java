58fed7934ae16cf9850e20c43a2bda00550d3f95
Ozan Argıt Önçeken <81165247+OzanArgitOnceken@users.noreply.github.com>
2021-06-06 16:53:39 +0300

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,5 +1,5 @@
 public interface UserInterface {
-    public boolean login();
+    public boolean login(BinarySearchTree<UserInterface> users);
     public void editProfile();
     public void showFreeTime(Doctor doctor);
     public void editUserProfile(User user);
